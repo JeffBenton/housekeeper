d60d59d96d5b53533de8ea268fb829d4b3bea778
Jeff Benton <jeff@frogrock.net>
2020-02-11 20:15:23 -0500

    added homecontainer component

---
+++ src/Containers/HomeContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class HomeContainer extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            houses: []
+        }
+    }
+
+    render() {
+        return (
+            <div>HomeContainer</div>
+        )
+    }
+}

96b5908575bcf6bcb500c8a7ebddc17798079569
Jeff Benton <jeff@frogrock.net>
2020-02-11 20:04:06 -0500

    initialized loginform component

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class LoginForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <input type="submit" />
+            </form>
+        )
+    }
+}
+
+export default LoginForm;

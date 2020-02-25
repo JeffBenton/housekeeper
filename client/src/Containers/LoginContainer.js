941b9ef7a6cdbff9a22f8363d341a14260850479
Jeff Benton <jeff@frogrock.net>
2020-02-25 18:45:53 -0500

    added react router routing

--- client/src/Containers/LoginContainer.js
+++ client/src/Containers/LoginContainer.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import RegisterForm from '../Components/RegisterForm'
+import LoginForm from '../Components/LoginForm'
+
 
 class LoginContainer extends React.Component {
     render() {
         return (
             <div>
-                <RegisterForm />
+                <Switch>
+                    <Route path="/">
+                        <LoginForm />
+                    </Route>
+
+                    <Route path="/register">
+                        <RegisterForm />
+                    </Route>
+                </Switch>
+
             </div>
         )
     }

77dfe653276d27b44073b7b86a7ee58e29206dd1
Jeff Benton <jeff@frogrock.net>
2020-02-25 18:17:08 -0500

    initialized logincontainer and registerform components

---
+++ client/src/Containers/LoginContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegisterForm from '../Components/RegisterForm'
+
+class LoginContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <RegisterForm />
+            </div>
+        )
+    }
+}
+
+export default LoginContainer;

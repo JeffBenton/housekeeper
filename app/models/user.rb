422fa75eca3c103b6f26a02e5324df5dfe037c3d
Jeff Benton <jeff@frogrock.net>
2020-03-17 20:59:36 -0400

    added validators to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true, format: { with: URI::MAILTO::EMAIL_REGEXP}, presence: true
+  validates :password, presence: true, on: create
 end
